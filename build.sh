7ad39502eba2240bec645a9bea8d9bc1a4841f14
Ian Finch <ianfinch@users.noreply.github.com>
2020-06-25 17:47:11 +0000

    Added bower dependencies

--- build.sh
+++ build.sh
@@ -4,5 +4,12 @@ git clone https://github.com/bramp/js-sequence-diagrams.git
 bower install js-sequence-diagrams
 
 mkdir public
+
 cp index.html public/
 cp style.css public/
+
+cp bower_components/bower-webfontloader/webfont.js public/
+cp bower_components/snap.svg/dist/snap.svg-min.js public/
+cp bower_components/underscore/underscore-min.js public/
+cp bower_components/js-sequence-diagrams/dist/sequence-diagram-min.js public/
+cp bower_components/js-sequence-diagrams/dist/sequence-diagram-min.css public/
